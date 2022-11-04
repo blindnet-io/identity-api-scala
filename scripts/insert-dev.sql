b093b2f322154a45705de9bd9e740983855aa835
m4rk055 <marko@blindnet.io>
2022-11-04 10:08:56 +0100

    Create tables for accounts and app groups

--- scripts/insert-dev.sql
+++ scripts/insert-dev.sql
@@ -1,1 +1,14 @@
-insert into applications values ('6f083c15-4ada-4671-a6d1-c671bc9105dc', 'test', '');
+insert into accounts values ('c1f296e9-8abd-467b-a064-d631a1bcc350');
+
+insert into application_groups (id, accid, name, key) values (
+  '9a222fb7-7fb8-45e8-b5ac-67a7b503ea33',
+  'c1f296e9-8abd-467b-a064-d631a1bcc350',
+  'test_group',
+  '2FY5BrVnN29UGo4X9I34zqDeLitHhr/tNBVk/BjmNOQ='
+);
+
+insert into applications (id, gid, name) values (
+  '6f083c15-4ada-4671-a6d1-c671bc9105dc',
+  '9a222fb7-7fb8-45e8-b5ac-67a7b503ea33',
+  'test_app'
+);
